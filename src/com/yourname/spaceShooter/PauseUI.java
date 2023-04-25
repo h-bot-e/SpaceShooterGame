369950cdc47f04ee287c00c3e92abef0118f0a20
howlingtaco <Yelitza.Pagan09@gmail.com>
2023-04-24 23:09:36 -0400

    PauseUI and Title UI
    Reverted PauseUI code back to original state. Formatted Title UI centered and added extra boxes.

--- src/com/yourname/spaceShooter/PauseUI.java
+++ src/com/yourname/spaceShooter/PauseUI.java
@@ -25,15 +25,45 @@ public class PauseUI {
 		g.setColor(Color.LIGHT_GRAY);
 		g.drawString("PAUSED",(int) (Game.WIDTH / 1.5), 100);
 		
-		//Buttons for Pause Menu
-		g2d.draw(resumeB);
+		
+	    //Buttons for Pause Menu
+	    
+	    //Resume Button
+	    g2d.setColor(Color.DARK_GRAY);
+	    g2d.fillRect(resumeB.x, resumeB.y, resumeB.width, resumeB.height);    
+		g2d.draw(resumeB);	
+        g2d.setColor(Color.WHITE);
+        g2d.setFont(new Font("Serial Roundhand", Font.BOLD, 25));
+        g2d.drawString("Resume", resumeB.x + 30, resumeB.y + 35);
+        
+        //Options Button
+        g2d.setColor(Color.DARK_GRAY);
+	    g2d.fillRect(optionsB.x, optionsB.y, optionsB.width, optionsB.height);    
 		g2d.draw(optionsB);
+        g2d.setColor(Color.WHITE);
+        g2d.setFont(new Font("Serial Roundhand", Font.BOLD, 25));
+        g2d.drawString("Options", optionsB.x + 30, optionsB.y + 35);
+        
+        //Back to Title Screen Button
+        g2d.setColor(Color.DARK_GRAY);
+	    g2d.fillRect(backB.x, backB.y, backB.width, backB.height);  
 		g2d.draw(backB);
+        g2d.setColor(Color.WHITE);
+        g2d.setFont(new Font("Serial Roundhand", Font.BOLD, 16));
+        g2d.drawString("Exit to Main Menu", backB.x + 10, backB.y + 35);
+		
+		//Exit to Desktop Button
+        g2d.setColor(Color.DARK_GRAY);
+	    g2d.fillRect(exitB.x, exitB.y, exitB.width, exitB.height); 
 		g2d.draw(exitB);
-	}
-	
-	
-	
-	
+        g2d.setColor(Color.WHITE);
+        g2d.setFont(new Font("Serial Roundhand", Font.BOLD, 16));
+        g2d.drawString("Exit to Desktop", exitB.x + 18, exitB.y + 35);
+		}
+//	}
+
+// public void setIsPaused(boolean isPaused) {
+//    this.isPaused = isPaused;
+// }
 	
 }
--- src/com/yourname/spaceShooter/TitleUI.java
+++ src/com/yourname/spaceShooter/TitleUI.java
@@ -7,8 +7,10 @@ import java.awt.Graphics2D;
 import java.awt.Rectangle;
 
 public class TitleUI {
-    public Rectangle playButton = new Rectangle(Game.WIDTH / 2 - 75, 300, 150, 50);
-
+    public Rectangle playButton = new Rectangle(Game.WIDTH / 2 + 140, 315, 200, 50);
+    public Rectangle settingsButton = new Rectangle(Game.WIDTH / 2 + 140, 415, 200, 50);
+    public Rectangle exitButton = new Rectangle(Game.WIDTH / 2 + 140, 515, 200, 50);
+    
     public void render(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
 
@@ -16,11 +18,27 @@ public class TitleUI {
         Font fnt = new Font("Serial Roundhand", Font.BOLD, 50);
         g.setFont(fnt);
         g.setColor(Color.LIGHT_GRAY);
-        g.drawString("Space Shooter", Game.WIDTH / 2 - 150, 200);
+        g.drawString("Space Shooter", (int) (Game.WIDTH / 1.7), 280);
 
         // Play button
         g2d.draw(playButton);
-        g.drawString("Play", playButton.x + 45, playButton.y + 35);
+        Font playfnt = new Font("Serial Roundhand", Font.BOLD, 45);
+        g.setFont(playfnt);
+        g.drawString("Play", playButton.x + 55, playButton.y + 40);
+    
+        //Settings Button
+        g2d.draw(settingsButton);
+        Font settingsfnt = new Font("Serial Roundhand", Font.BOLD, 45);
+        g.setFont(settingsfnt);
+        g.drawString("Settings", settingsButton.x + 15, settingsButton.y + 40);
+        
+        //Exit Button
+        g2d.draw(exitButton);
+        Font exitfnt = new Font("Serial Roundhand", Font.BOLD, 25);
+        g.setFont(exitfnt);
+        g.drawString("Exit to Desktop", exitButton.x + 10, exitButton.y + 35);
+        
+    
     }
 
     public boolean isPlayButtonClicked(int mouseX, int mouseY) {
